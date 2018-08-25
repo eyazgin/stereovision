7e243012d7b2809111d787f8ac3cb50b3c9fbc28
eyazgin <42637365+eyazgin@users.noreply.github.com>
2018-08-25 11:18:16 +1000

    Update playingwitharrays2.m

--- playingwitharrays2.m
+++ playingwitharrays2.m
@@ -1,13 +1,12 @@
-A = [1 1 1 0 0 3];
-B = [2 1 1 1 1 1];
-    % answer is [0 1 2 1 0 1]
-% we want to find the highest cumulative value for the multiplication of the
-    % arrays
+A = [1 1 1 1 1 1];
+B = [1 2 3 4 5 6];
+
 length(A);
 padding = zeros(1,length(A)-1);
 padded_A = [ padding , A , padding ];
 padded_B = [ padding , B , padding ];
 
 for n=1:length(A)+length(padding)
-    padded_A(1:length(padding)+n)
+    moving_A=padded_A(n:length(padding)+n)
+    moving_A.*B
 end
