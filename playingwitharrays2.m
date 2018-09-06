a20bf6bf5fbbe6f83071375acbb93eed78b78df9
eyazgin <42637365+eyazgin@users.noreply.github.com>
2018-09-06 15:37:40 +1000

    Update playingwitharrays2.m

--- playingwitharrays2.m
+++ playingwitharrays2.m
@@ -1,21 +1,18 @@
-A = [1 1 1 1 1 1];
-B = [1 2 3 4 5 6];
+A = [0 0 1 1 0 0];
+B = [0 0 0 1 1 0];
 
 length(A);
 padding = zeros(1,length(A)-1);
 padded_A = [ padding , A , padding ];
 padded_B = [ padding , B , padding ];
 
-for n=1:length(A)+length(padding)
+for n=1:length(A)+length(padding);
     moving_A = padded_A(n:length(padding)+n);
-    moving_A.*B;
     sum_moving_A = sum(moving_A.*B);
-
+    correlation_array(n) = [ sum_moving_A ];
+    
 end
 
-correlation_array(n) = sum_moving_A;
+correlation_array
 max_correlation = max(correlation_array(:))
 [row,column] = find(ismember(correlation_array, max(correlation_array(:))))
-
-if max_correlation < max(correlation_array(:))
-    then
