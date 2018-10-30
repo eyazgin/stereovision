672fa74878ccf00de1e619808ba50c95b08700b2
eyazgin <42637365+eyazgin@users.noreply.github.com>
2018-10-30 15:18:15 +1100

    Update playingwitharrays2.m

--- playingwitharrays2.m
+++ playingwitharrays2.m
@@ -7,9 +7,11 @@ mean_B = (max(B) - min(B))/2;
 offset_A = A - min(A) - mean_A;
 offset_B = B - min(B) - mean_B;
 
-variance_offset_A = sqrt(((sum(offset_A.^2)))/length(A))
-variance_offset_B = sqrt(((sum(offset_B.^2)))/length(B))
-variance_overall = variance_offset_A*variance_offset_B
+variance_offset_A = sqrt((((sum(offset_A.^2))))/length(A));
+variance_offset_B = sqrt((((sum(offset_B.^2))))/length(B));
+
+norm_offset_A = offset_A/variance_offset_A;
+norm_offset_B = offset_B/variance_offset_B;
 
 % CROSS CORRELATION:
 
@@ -17,12 +19,12 @@ variance_overall = variance_offset_A*variance_offset_B
 
 length(A);
 padding = zeros(1,length(A)-1);
-padded_norm_offset_A = [ padding , offset_A , padding ];
-padded_norm_offset_B = [ padding , offset_B , padding ];
+padded_norm_offset_A = [ padding , norm_offset_A , padding ];
+padded_norm_offset_B = [ padding , norm_offset_B , padding ];
 
 for n=1:length(A)+length(padding);
     moving_offset_A = padded_norm_offset_A(n:length(padding)+n);
-    sum_moving_offset_A = sum(moving_offset_A.*offset_B);
+    sum_moving_offset_A = sum(moving_offset_A.*norm_offset_B);
     correlation_array(n) = [ sum_moving_offset_A ];
     
 end
@@ -33,4 +35,4 @@ max_correlation = max(correlation_array(:))
 
 % NORMALISED CROSS CORRELATION:
 
-norm_correlation_array = correlation_array/variance_overall
+norm(correlation_array);
