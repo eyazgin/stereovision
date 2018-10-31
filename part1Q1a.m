8ac156706c944eac18209b8cb6034a12fe298100
eyazgin <42637365+eyazgin@users.noreply.github.com>
2018-10-31 13:11:20 +1100

    Update and rename part1Q1.m to part1Q1a.m

---
+++ part1Q1a.m
@@ -0,0 +1,24 @@
+% CROSS CORRELATION:
+
+% Below is the correlation for vectors A & B
+
+A = [0 0 1 1 0 0];
+B = [0 0 0 1 1 0];
+
+length(A);
+padding = zeros(1,length(A)-1);
+padded_A = [ padding , A , padding ];
+padded_B = [ padding , B , padding ];
+
+for n=1:length(A)+length(padding);
+    moving_A = padded_A(n:length(padding)+n);
+    sum_moving_A = sum(moving_A.*B);
+    correlation_array(n) = [ sum_moving_A ];
+    
+end
+
+correlation_array
+max_correlation = max(correlation_array(:));
+distance_moved = find(ismember(correlation_array, max(correlation_array(:))))
+
+distance_separation = abs(length(B)-distance_moved)
