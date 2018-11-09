f9a245742329c19fd6594c62a743bb97b6bfff72
eyazgin <42637365+eyazgin@users.noreply.github.com>
2018-11-09 14:36:55 +1100

    Update part1Q1b.m

--- part1Q1b.m
+++ part1Q1b.m
@@ -2,31 +2,35 @@
 
 % Below is the correlation for normalised vectors A & B
 
-A = [0 0 1 1 0 0];
-B = [0 0 0 1 1 0];
+B = [1 1 2 2 1 1];
+A = [1 1 1 2 2 1];
 
-mean_A = (max(A) - min(A))/2;
-mean_B = (max(B) - min(B))/2;
+mean_A = sum(A)/length(A);
+mean_B = sum(B)/length(B);
 
-offset_A = A - min(A) - mean_A;
-offset_B = B - min(B) - mean_B;
+std_A = std(A);
+std_B = std(B);
 
-variance_offset_A = sqrt((((sum(offset_A.^2))))/length(A));
-variance_offset_B = sqrt((((sum(offset_B.^2))))/length(B));
+offset_A = A - mean_A;
+offset_B = B - mean_B;
 
-norm_offset_A = offset_A/variance_offset_A;
-norm_offset_B = offset_B/variance_offset_B;
+norm_offset_A = offset_A/std_A;
+norm_offset_B = offset_B/std_B;
 
 length(A);
 padding = zeros(1,length(A)-1);
-padded_norm_offset_A = [ padding , norm_offset_A , padding ];
-padded_norm_offset_B = [ padding , norm_offset_B , padding ];
+padded_A = [ padding , norm_offset_A , padding ];
+padded_B = [ padding , norm_offset_B , padding ];
 
-for n=1:length(A)+length(padding);
-    moving_offset_A = padded_norm_offset_A(n:length(padding)+n);
-    sum_moving_offset_A = sum(moving_offset_A.*norm_offset_B);
-    correlation_array(n) = [ sum_moving_offset_A ];
-    
+
+for n=1:length(A)+length(padding)
+    n;
+    moving_offset_A = padded_A(length(A):length(padding)+n);
+    moving_offset_B = padded_B(length(padding)+length(B)+1-n:length(padding)+length(B));
+       
+    sum_moving_offset_A = (1/(length(B)))*sum(moving_offset_A.*moving_offset_B);
+       
+    correlation_array(n) = [ sum_moving_offset_A ];    
 end
 
 correlation_array
